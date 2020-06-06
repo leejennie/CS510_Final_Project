1f2852235b50d6f8182ca6b012d554846c3874b3
Jennie <ljennie@pdx.edu>
2020-06-05 18:21:55 -0700

    stats.js

--- stats.js
+++ stats.js
@@ -1,11 +1,14 @@
 //Tutorial came from: https://github.com/dimestorerocket/html5-javascript-bar-chart-from-csv-file/blob/master/BarGraphTempData.html
 
+//First csv file 
 function makeChart(states) {
-
+    
+    //Grab the data to be stored in objectrs 
     var statesLabel = states.map(function(d){return d.State});
     var dailysales = states.map(function(d) {return d.DailySalesRevenue});
     var avgtrans = states.map(function(d) {return d.AverageTransactions});
 
+    //Create the first graph
     var chart = new Chart('chart', {
       type: 'bar',
       data: {
@@ -57,6 +60,7 @@ function makeChart(states) {
       }
     });
 
+  //Create second graph   
   var chart2 = new Chart('chart2', {
     type: 'bar',
     data: {
@@ -109,14 +113,17 @@ function makeChart(states) {
   });
 }
 
+//Function with the second csv file
 function makeChart2(cities) {
 
+    //read in the information from the csv file and store in objects
     var citiesLabel = cities.map(function(d){return d.City});
     var ttt = cities.map(function(d) {return d.TableTurnTime});
     var qsr = cities.map(function(d) {return d.QSRAvgTicketSize});
     var fsr = cities.map(function(d) {return d.FSRAvgTicketSize});
     var pop = cities.map(function(d) {return d.PercentOffPremise});
 
+    //create third graph
     var chart3 = new Chart('chart3', {
       type: 'bar',
       data: {
@@ -168,7 +175,8 @@ function makeChart2(cities) {
     }
   });
 
-var chart4 = new Chart('chart4', {
+    //Create fourth graph 
+    var chart4 = new Chart('chart4', {
       type: 'bar',
       data: {
         labels: citiesLabel,
@@ -218,58 +226,8 @@ var chart4 = new Chart('chart4', {
       }
     }
   });
-
-var chart4 = new Chart('chart4', {
-      type: 'bar',
-      data: {
-        labels: citiesLabel,
-        datasets: [
-          {
-            data: pop,
-            backgroundColor: 'rgb(220, 53, 69, 0.8)'
-          }
-        ]
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'Percent off Premise Popular Cities in the US',
-          fontSize: 18
-        },
-        legend: {
-          display: false
-        },
-        scales: {
-          yAxes: [
-          {
-            scaleLabel: {
-              display: true,
-              labelString: 'Percent Off Premise (%)',
-              fontSize: 16
-            },
-            ticks: {
-              min: 0,
-              stepSize: 2,
-              fontSize: 14
-            }
-          }
-        ],
-          xAxes: [
-          {
-            scaleLabel: {
-              display: true,
-              labelString: 'Cities',
-              fontSize: 16
-            },
-            ticks: {
-              fontSize: 14
-            }
-          }
-        ]
-      }
-    }
-  });
-
+   
+  //Create fifth graph. Double bar graph.
   var chart5 = new Chart('chart5', {
     type: 'bar',
     data: {
@@ -329,7 +287,7 @@ var chart4 = new Chart('chart4', {
 
 }
 
-
+//Use d3 to read and store the csv files
 var file1 = 'https://raw.githubusercontent.com/leejennie/leejennie.github.io/master/state_sales_revenue.csv';
 d3.csv(file1).then(makeChart);
 
